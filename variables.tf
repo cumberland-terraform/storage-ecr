861f0977ec01d0b3402324d5cf203a7a07ec4752
Grant Moore <gmoore@Grants-MacBook-Pro.local>
2025-07-27 19:34:17 -0400

    iupdate platform variable

--- variables.tf
+++ variables.tf
@@ -1,15 +1,11 @@
 variable "platform" {
-  description                   = "Platform metadata configuration object. See [Platform Module] (https://source.mdthink.maryland.gov/projects/etm/repos/mdt-eter-platform/browse) for detailed information about the permitted values for each field."
-  type                          = object({
-    aws_region                  = string 
-    account                     = string
-    acct_env                    = string
-    agency                      = string
-    program                     = string
-    app_env                     = string
-    pca                         = string
+  description                                   = "Platform metadata configuration object."
+  type                                          = object({
+    client                                      = string 
+    environment                                 = string
   })
 }
+
 
 variable "ecr" {
   description                   = "ECR configuration object. See [README](https://source.mdthink.maryland.gov/projects/etm/repos/mdt-eter-core-storage-ecr/browse) for detailed information about the permitted values for each field."
